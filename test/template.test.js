6af3a0c5a4df9ee349b58387587fa1c3cae50e18
believeitcould <believeitcould@sina.com>
2017-02-25 23:13:04 +0800

    update test

--- test/template.test.js
+++ test/template.test.js
@@ -3,6 +3,9 @@ var expect = require('chai').expect
 
 describe('模板文件加载测试', function () {
     it('无分号', function () {
-        expect(loadTemplate(undefined, 'Test')).to.not.match(/;/)
+        expect(loadTemplate(undefined, 'Test')).to.not.match(/;/g)
+    })
+    it('有分号', function () {
+        expect(loadTemplate(true, 'Test')).to.match(/;/g)
     })
 })
